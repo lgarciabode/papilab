d19001333c7366efc80bc4d415812e5079d2bd1e
lgarciabode <lgarciabode@gamil.com>
2025-08-07 18:33:42 -0300

    Maquetado inicial

--- app.py
+++ app.py
@@ -1,27 +1,39 @@
 import streamlit as st
 from PIL import Image
+import os
+import random
 
 st.set_page_config(page_title="PapiLab Educativo", layout="centered")
 
 st.title("🧬 Papilab - Clasificación de Patrones Papilares")
 st.write("Este es un espacio interactivo para practicar la identificación de patrones papilares")
 
-#Muestra la imagen de una huella
-img = Image.open("huellas/huella1.png")
-img = img.resize((300, 400))  # ancho x alto en píxeles
-st.image(img, caption="Huella dactilar 1")
+#Selector de area
+area = st.selectbox("¿En qué área deseas practicar?", ["Dactilar", "Palmar", "Plantar"]
+)
+
+folder = f"huellas/{area.lower()}es"
+imagenes = os.listdir(folder)
+imagen_seleccionada = random.choice(imagenes)
+st.image(f"{folder}/{imagen_seleccionada}",  width=400) #use_container_width=True,
+
 
 #Seleccion de patron
-opcion = st.selectbox(
+tipo_patron = st.selectbox(
     "¿Qué patrón papilar identificas en la huella?",
     ["Seleccionar", "Arco", "Presilla interna", "Presilla externa", "Verticilo"]
 )
-st.write(f"Elegiste: {opcion}")
+st.write(f"Elegiste: {tipo_patron}")
 
 
 #Evalua la respuesta
-if opcion != "Seleccionar":
-    if opcion == "Verticilo":
+if tipo_patron != "Seleccionar":
+    if tipo_patron == "Verticilo":
         st.success("✅ ¡Correcto! Esta huella es un verticilo.")
     else:
         st.error("❌ Incorrecto. La respuesta correcta es: Verticilo.")
+
+if tipo_patron == "Arco":
+        subclasificacion = st.selectbox("Subclasificación (Arco)", ["6", "7", "8", "9"])
+elif tipo_patron in ["Presilla", "Verticilo"]:
+        subclasificacion = st.selectbox(f"Subclasificación ({tipo_patron})", ["S", "D", "M"])
